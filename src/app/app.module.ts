3d9c56a1ee22bb656a7461d3b1b537b9f52ef0d8
oscar-quiroz <kairox.rt@gmail.com>
2021-01-24 10:59:04 -0500

    cambio titulo index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './componentes/nav-bar/nav-bar.component';
+import { GaleryComponent } from './componentes/galery/galery.component';
+import { CardComponent } from './componentes/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    GaleryComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
