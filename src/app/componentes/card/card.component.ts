23b7d37943e69dd6370263fa97fded2308948690
oscar-quiroz <kairox.rt@gmail.com>
2021-01-24 16:30:05 -0500

    añadiendo datos de imagenes

--- src/app/componentes/card/card.component.ts
+++ src/app/componentes/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input} from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+
+  @Input() img:string = "";
+  @Input() desc:string = "";
   constructor() { }
 
   ngOnInit(): void {
--- src/app/componentes/galery/galery.component.ts
+++ src/app/componentes/galery/galery.component.ts
@@ -7,7 +7,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GaleryComponent implements OnInit {
 
-  constructor() { }
+  images:any;
+
+  constructor() { 
+    
+    this.images = [
+      {
+        "img": "https://scontent-bog1-1.cdninstagram.com/v/t51.2885-15/e35/103413265_2683109511958138_7449347726880263576_n.jpg?_nc_ht=scontent-bog1-1.cdninstagram.com&_nc_cat=110&_nc_ohc=xxZooZBfdBQAX-plJaC&tp=1&oh=e4bf815f6a6ebf52c263a08cf2d255c6&oe=6037B7C6",
+        "valor": "55000"
+      },
+      {
+        "img": "https://scontent-bog1-1.cdninstagram.com/v/t51.2885-15/e35/103152955_879044675931567_4028597452015048587_n.jpg?_nc_ht=scontent-bog1-1.cdninstagram.com&_nc_cat=103&_nc_ohc=xn5ttvbqff8AX_QI3fx&tp=1&oh=241fe1bae014f6c81f9de66231346a28&oe=6036B2C2",
+          "valor": "40000"
+      },
+      {
+        "img": "https://scontent-bog1-1.cdninstagram.com/v/t51.2885-15/e35/103152955_879044675931567_4028597452015048587_n.jpg?_nc_ht=scontent-bog1-1.cdninstagram.com&_nc_cat=103&_nc_ohc=xn5ttvbqff8AX_QI3fx&tp=1&oh=241fe1bae014f6c81f9de66231346a28&oe=6036B2C2",
+        "valor": "40000"
+      },
+      {
+        "img": "https://scontent-bog1-1.cdninstagram.com/v/t51.2885-15/e35/103413265_2683109511958138_7449347726880263576_n.jpg?_nc_ht=scontent-bog1-1.cdninstagram.com&_nc_cat=110&_nc_ohc=xxZooZBfdBQAX-plJaC&tp=1&oh=e4bf815f6a6ebf52c263a08cf2d255c6&oe=6037B7C6",
+        "valor": "55000"
+      },
+      {
+        "img": "https://scontent-bog1-1.cdninstagram.com/v/t51.2885-15/e35/103152955_879044675931567_4028597452015048587_n.jpg?_nc_ht=scontent-bog1-1.cdninstagram.com&_nc_cat=103&_nc_ohc=xn5ttvbqff8AX_QI3fx&tp=1&oh=241fe1bae014f6c81f9de66231346a28&oe=6036B2C2",
+        "valor": "40000"
+      },{
+        "img": "https://scontent-bog1-1.cdninstagram.com/v/t51.2885-15/e35/103152955_879044675931567_4028597452015048587_n.jpg?_nc_ht=scontent-bog1-1.cdninstagram.com&_nc_cat=103&_nc_ohc=xn5ttvbqff8AX_QI3fx&tp=1&oh=241fe1bae014f6c81f9de66231346a28&oe=6036B2C2",
+        "valor": "40000"
+      },{
+        "img": "https://scontent-bog1-1.cdninstagram.com/v/t51.2885-15/e35/103152955_879044675931567_4028597452015048587_n.jpg?_nc_ht=scontent-bog1-1.cdninstagram.com&_nc_cat=103&_nc_ohc=xn5ttvbqff8AX_QI3fx&tp=1&oh=241fe1bae014f6c81f9de66231346a28&oe=6036B2C2",
+        "valor": "40000"
+      },
+      {
+        "img": "https://scontent-bog1-1.cdninstagram.com/v/t51.2885-15/e35/103152955_879044675931567_4028597452015048587_n.jpg?_nc_ht=scontent-bog1-1.cdninstagram.com&_nc_cat=103&_nc_ohc=xn5ttvbqff8AX_QI3fx&tp=1&oh=241fe1bae014f6c81f9de66231346a28&oe=6036B2C2",
+        "valor": "40000"
+      },
+      {
+        "img": "https://scontent-bog1-1.cdninstagram.com/v/t51.2885-15/e35/103152955_879044675931567_4028597452015048587_n.jpg?_nc_ht=scontent-bog1-1.cdninstagram.com&_nc_cat=103&_nc_ohc=xn5ttvbqff8AX_QI3fx&tp=1&oh=241fe1bae014f6c81f9de66231346a28&oe=6036B2C2",
+        "valor": "40000"
+      },
+      {
+        "img": "https://scontent-bog1-1.cdninstagram.com/v/t51.2885-15/e35/103152955_879044675931567_4028597452015048587_n.jpg?_nc_ht=scontent-bog1-1.cdninstagram.com&_nc_cat=103&_nc_ohc=xn5ttvbqff8AX_QI3fx&tp=1&oh=241fe1bae014f6c81f9de66231346a28&oe=6036B2C2",
+        "valor": "40000"
+      },
+      {
+        "img": "https://scontent-bog1-1.cdninstagram.com/v/t51.2885-15/e35/103152955_879044675931567_4028597452015048587_n.jpg?_nc_ht=scontent-bog1-1.cdninstagram.com&_nc_cat=103&_nc_ohc=xn5ttvbqff8AX_QI3fx&tp=1&oh=241fe1bae014f6c81f9de66231346a28&oe=6036B2C2",
+        "valor": "40000"
+      }
+  ]
+  }
 
   ngOnInit(): void {
   }
