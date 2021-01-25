19dee9a0db1cbb0df0fb334c614b267fc28073eb
oscar-quiroz <kairox.rt@gmail.com>
2021-01-24 23:16:53 -0500

    implmentacion de modal

--- src/app/componentes/card/card.component.ts
+++ src/app/componentes/card/card.component.ts
@@ -1,18 +1,32 @@
-import { Component, OnInit,Input} from '@angular/core';
+import { Component, OnInit,Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
+
+
+
+
 export class CardComponent implements OnInit {
 
+ 
 
   @Input() img:string = "";
-  @Input() desc:string = "";
+  @Input() valor:string = "";
+
+  @Output() mostrar:EventEmitter<string> = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  mostrarDatos(){
+    console.log(this.img)
+    this.mostrar.emit(this.img );
+  }
+
 }
+
